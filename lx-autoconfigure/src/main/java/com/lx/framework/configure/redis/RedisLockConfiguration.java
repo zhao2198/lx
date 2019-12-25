e57bffb8e17c7bf51c9517307c2aa9b741615e7b
zhaowei <zhaowei@sancaijia.com>
2019-12-25 18:06:06 +0800

    add lock

---
+++ lx-autoconfigure/src/main/java/com/lx/framework/configure/redis/RedisLockConfiguration.java
@@ -0,0 +1,27 @@
+package com.lx.framework.configure.redis;
+
+
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.integration.redis.util.RedisLockRegistry;
+
+/**
+ * 并发锁
+ *
+ * @Project RedisLockConfiguration(com.lx.framework.configure.redis)
+ * @Author  zhaowei
+ * @Date    2019/12/25 17:10
+ * @Version v1.1.0
+ */
+
+@Configuration
+@AutoConfigureAfter(RedisCacheAutoConfiguration.class)
+public class RedisLockConfiguration {
+
+    @Bean
+    public RedisLockRegistry redisLockRegistry(RedisConnectionFactory redisConnectionFactory) {
+        return new RedisLockRegistry(redisConnectionFactory, "spring-cloud");
+    }
+}
--- lx-enterprise/lx-user/src/main/java/com/lx/user/controller/UserController.java
+++ lx-enterprise/lx-user/src/main/java/com/lx/user/controller/UserController.java
@@ -8,13 +8,17 @@ import com.lx.user.dto.UserQueryDTO;
 import com.lx.user.entity.User;
 import com.lx.user.service.UserService;
 import com.lx.user.vo.UserVO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.integration.redis.util.RedisLockRegistry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
 
 /**
  * <p>
@@ -26,10 +30,14 @@ import java.util.List;
  */
 @ApiV1Controller
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    private RedisLockRegistry redisLockRegistry;
 
     @GetMapping("/{id}")
     ResponseEntity getUser(@PathVariable("id") Long id) {
@@ -43,5 +51,43 @@ public class UserController {
         List<UserVO> voList = userService.getUserList(user);
         return new ApiResponse().ok(voList);
     }
+
+
+    @GetMapping("/lock")
+    ResponseEntity lock() throws InterruptedException {
+
+        Lock lock = redisLockRegistry.obtain("lock");
+        boolean b1 = lock.tryLock(10, TimeUnit.SECONDS);
+        log.info("b1 is : {}", b1);
+
+        TimeUnit.SECONDS.sleep(5);
+
+        boolean b2 = lock.tryLock(3, TimeUnit.SECONDS);
+        log.info("b2 is : {}", b2);
+
+        lock.unlock();
+        lock.unlock();
+        return new ApiResponse().ok();
+    }
+
+    @GetMapping("/lock1")
+    ResponseEntity lock1() throws InterruptedException {
+
+        Lock lock = redisLockRegistry.obtain("lock");
+        boolean b1 = lock.tryLock(3, TimeUnit.SECONDS);
+        log.info("b1 is : {}", b1);
+
+        TimeUnit.SECONDS.sleep(5);
+
+        boolean b2 = lock.tryLock(3, TimeUnit.SECONDS);
+        log.info("b2 is : {}", b2);
+
+        lock.unlock();
+        lock.unlock();
+        return new ApiResponse().ok();
+    }
+
+
+
 
 }
